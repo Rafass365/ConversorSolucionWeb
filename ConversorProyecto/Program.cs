9a47721a22a6b0ceeab252cb7c126605319c9e19
Rafael Suarez Soria <rafael.suarezsoria@tajamar365.com>
2023-05-05 19:23:10 +0200

    Agregar archivos de proyecto.

---
+++ ConversorProyecto/Program.cs
@@ -0,0 +1,22 @@
+namespace ConversorProyecto
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            //app.MapGet("/", () => "Hello World!");
+            //app.UseWelcomePage();
+            app.UseStaticFiles();       //
+
+
+
+
+
+            app.Run();
+            //Console.WriteLine("Rafaelito´s");
+        }
+    }
+}
