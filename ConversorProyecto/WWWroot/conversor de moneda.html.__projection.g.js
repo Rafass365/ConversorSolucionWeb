c55aa58f322249708007881f8c3fcfbf620c1479
Rafael Suarez Soria <rafael.suarezsoria@tajamar365.com>
2023-05-09 20:07:11 +0200

    09052023 V.1

---
+++ ConversorProyecto/WWWroot/conversor de moneda.html.__projection.g.js
@@ -0,0 +1,10 @@
+/* BEGIN EXTERNAL SOURCE */
+function name3() {
+selecciona_moneda_origen()
+}
+/* END EXTERNAL SOURCE */
+/* BEGIN EXTERNAL SOURCE */
+function name4() {
+selecciona_moneda_origen()
+}
+/* END EXTERNAL SOURCE */
---
+++ ConversorProyecto/WWWroot/index.html.__projection.g.js
@@ -0,0 +1,10 @@
+/* BEGIN EXTERNAL SOURCE */
+function name1() {
+selecciona_moneda_origen()
+}
+/* END EXTERNAL SOURCE */
+/* BEGIN EXTERNAL SOURCE */
+function name2() {
+selecciona_moneda_destino()
+}
+/* END EXTERNAL SOURCE */
